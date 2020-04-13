3ccb1bcc2506816d504d154e60dfe1172db0e363
thiagodevx <thiago.dev.x@gmail.com>
2020-04-13 18:48:29 -0300

    formatação do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
+const port = 3333
 
 app.get('/', (req, res) => {
-    res.json({message: 'Hello world'});
-});
-const port = 3333;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+  res.json({ message: 'Hello world' })
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
