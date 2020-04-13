16925cbf2f5c3acd85dcf1d627e38c977dfba5d2
thiagodevx <thiago.dev.x@gmail.com>
2020-04-13 16:45:50 -0300

    evoluções para o script de dev que você desabilita a checagem se o código esta correto ou não

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,5 @@ const app = express();
 app.get('/', (req, res) => {
     res.json({message: 'Hello world'});
 });
-app.listen(3333, () => console.log('Server started on port 3333'));
+const port = 3333;
+app.listen(port, () => console.log(`Server started on port ${port}`));
