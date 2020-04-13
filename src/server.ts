f88ed3d55948764f01c5235c8e6c2bc1498f738d
thiagodevx <thiago.dev.x@gmail.com>
2020-04-13 16:40:23 -0300

    Updating server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-import express from 'express'
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello world'});
+});
+app.listen(3333, () => console.log('Server started on port 3333'));
