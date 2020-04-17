5d56f22fe14fe56eca06e6922ea55d51502eb120
thiagodevx <thiago.dev.x@gmail.com>
2020-04-17 19:33:47 -0300

    tipagens no retorno

--- src/appointments/appointments.repository.ts
+++ src/appointments/appointments.repository.ts
@@ -5,15 +5,15 @@ import { isEqual } from 'date-fns'
 export default class AppointmentsRepository {
   private appointments: Appointment[] = []
 
-  public create(appointment: Appointment) {
+  public create(appointment: Appointment): Appointment {
     const savedAppointment = { id: uuid(), ...appointment }
     this.appointments.push(savedAppointment)
     return savedAppointment
   }
-  public findAll() {
+  public findAll(): Appointment[] {
     return [...this.appointments]
   }
-  public findByDate(date: Date) {
+  public findByDate(date: Date): Appointment | undefined {
     return this.appointments.find(appointment =>
       isEqual(appointment.date, date)
     )
