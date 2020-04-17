2fc371e809afd60702295f8fae76a48a099b14ef
thiagodevx <thiago.dev.x@gmail.com>
2020-04-17 19:19:15 -0300

    criação da entidade appointment

--- src/appointments/appointment.model.ts
+++ src/appointments/appointment.model.ts
@@ -1,4 +1,8 @@
+import { uuid } from 'uuidv4'
+
 export default class Appointment {
-  id?: string
-  constructor(public provider: string, public date: Date) {}
+  id: string
+  constructor(public provider: string, public date: Date) {
+    this.id = uuid()
+  }
 }
--- src/appointments/appointment.routes.ts
+++ src/appointments/appointment.routes.ts
@@ -1,23 +1,20 @@
 import { Router } from 'express'
-import { uuid } from 'uuidv4'
 import { startOfHour, parseISO, isEqual } from 'date-fns'
+import Appointment from './appointment.model'
 
 const appointmentsRouter = Router()
 const appointments: Appointment[] = []
 
 appointmentsRouter.get('/', (request, response) => {
-  response.json({ teste: 'funcionando' })
+  response.json(appointments)
 })
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body
   const formatedDate = formatDate(date)
   if (validateAppointmentDate(formatedDate)) {
-    const appointment: Appointment = {
-      id: uuid(),
-      provider,
-      date: formatedDate
-    }
+    const appointment = new Appointment(provider, formatedDate)
+    appointments.push(appointment)
     response.json(appointment)
   } else {
     response.status(400).json({ message: 'Invalid Schedule' })
