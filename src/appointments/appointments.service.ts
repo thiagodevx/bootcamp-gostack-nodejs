d49283c4314305c4a21a998bea3dbd7e198309f9
thiagodevx <thiago.dev.x@gmail.com>
2020-04-19 18:59:11 -0300

    utilização de algumas tipagens

--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -5,27 +5,27 @@ import Appointment from './appointment.model'
 export default class AppointmentsService {
   private repository = new AppointmentsRepository()
 
-  public createAppointment = (provider: string, date: string) => {
+  public createAppointment = (provider: string, date: string): Appointment => {
     const formatedDate = this.formatDate(date)
     if (this.validateAppointmentDate(formatedDate)) {
       const appointment = new Appointment(provider, formatedDate)
       const savedAppointment = this.repository.create(appointment)
       return savedAppointment
     } else {
-      throw Error('invalid appointment date')
+      throw Error('Invalid appointment date')
     }
   }
 
-  public listAppointments = () => {
+  public listAppointments = (): Appointment[] => {
     return this.repository.findAll()
   }
 
-  private formatDate = (dateAsText: string) => {
+  private formatDate = (dateAsText: string): Date => {
     return startOfHour(parseISO(dateAsText))
   }
 
-  private validateAppointmentDate = (date: Date) => {
-    const result = this.repository.findByDate(date)
-    return !result
+  private validateAppointmentDate = (date: Date): Boolean => {
+    const found = this.repository.findByDate(date)
+    return !found
   }
 }
