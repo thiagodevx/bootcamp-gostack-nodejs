be51d9326a47c793c7658c1dc82bb80f84502bfd
thiagodevx <thiago.dev.x@gmail.com>
2020-04-19 20:56:34 -0300

    CreateAppointments or Appointments

---
+++ src/config/migrations/1587340500035-Appointments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class Appointments1587340500035 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {}
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
