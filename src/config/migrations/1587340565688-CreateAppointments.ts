11aa662268a840c02663fbe6e20a123b4bc80ece
thiagodevx <thiago.dev.x@gmail.com>
2020-04-19 21:24:10 -0300

    migration up

--- src/config/migrations/1587340565688-CreateAppointments.ts
+++ src/config/migrations/1587340565688-CreateAppointments.ts
@@ -1,7 +1,36 @@
-import { MigrationInterface, QueryRunner } from 'typeorm'
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+import { TableOptions } from 'typeorm/schema-builder/options/TableOptions'
 
-export class CreateAppointments1587340565688 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<any> {}
-
-  public async down(queryRunner: QueryRunner): Promise<any> {}
+const tableInfo: TableOptions = {
+  name: 'appointments',
+  columns: [
+    {
+      name: 'id',
+      type: 'varchar',
+      isPrimary: true,
+      generationStrategy: 'uuid'
+    },
+    {
+      name: 'provider',
+      type: 'varchar',
+      isNullable: false
+    },
+    {
+      name: 'date',
+      type: 'timestamp with time zone',
+      isNullable: false
+    }
+  ]
 }
+
+class CreateAppointments1587340565688 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(new Table(tableInfo))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('appointments')
+  }
+}
+
+export default CreateAppointments1587340565688
