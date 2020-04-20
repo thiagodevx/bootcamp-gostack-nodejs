200421ad6fa894c0273b0cd144eaf1b4db612d07
thiagodevx <thiago.dev.x@gmail.com>
2020-04-20 20:27:10 -0300

    servidor

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import 'reflect-metadata'
 import express from 'express'
 import routes from './config/routes'
 import './config/database'
+import multerConfiguration from './config/multerConfiguration'
 
 const app = express()
 app.use(express.json())
 app.use(routes)
+app.use('/files', express.static(multerConfiguration.avatarFolter))
 
 const port = 3333
 app.listen(port, () => console.log(`Server started on port ${port}`))
