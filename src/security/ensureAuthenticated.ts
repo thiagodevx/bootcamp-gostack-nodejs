74fe635fe1cc03778863c455fba44ab3c64796f4
thiagodevx <thiago.dev.x@gmail.com>
2020-04-20 19:05:51 -0300

    override da tipagem no express

--- src/security/ensureAuthenticated.ts
+++ src/security/ensureAuthenticated.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from 'express'
-import { verify } from 'jsonwebtoken'
 import AuthenticationService from './authentication.service'
 
 const service = new AuthenticationService()
 
-export default (request: Request, response: Response, next: NextFunction) => {
+export default async (request: Request, response: Response, next: NextFunction) => {
   try {
     const authHeader = request.headers.authorization
-    service.checkToken(authHeader)
+    const userId = await service.checkToken(authHeader)
+    request.user = { id: userId }
     next()
   } catch (e) {
     response.status(400).json({ message: e.message })
