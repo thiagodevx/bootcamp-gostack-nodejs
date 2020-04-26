d78616da2cab3b1a0fa57ed2cec6e617a84de9d0
thiagodevx <thiago.dev.x@gmail.com>
2020-04-25 23:16:27 -0300

    using children

--- web-gobarber/src/shared/Button.tsx
+++ web-gobarber/src/shared/Button.tsx
@@ -3,9 +3,10 @@ import './Button.scss'
 
 interface ButtonProps {
   children: string
+  type?: 'button' | 'submit'
 }
 export default (props: ButtonProps) => (
-  <button className='default-button' type='submit'>
+  <button className='default-button' type={props.type || 'button'}>
     {props.children}
   </button>
 )
