121c35558cf707ad4b8847f24ddedd2f057e1313
thiagodevx <thiago.dev.x@gmail.com>
2020-05-09 20:06:40 -0300

    alterações visuais

--- web-gobarber/src/shared/TooltipIcon.tsx
+++ web-gobarber/src/shared/TooltipIcon.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IconBaseProps } from 'react-icons/lib/cjs'
-
+import './TooltipIcon.scss'
 interface TooltipProps {
   icon: React.ComponentType<IconBaseProps>
   message: string
 }
 export default (props: TooltipProps) => {
   const Icon = props.icon
+  const [visible, setVisible] = useState(false)
+  const visibleClass = visible ? 'visible' : 'invisible'
   return (
-    <div>
+    <div className='tooltip-container' onMouseOver={() => setVisible(true)} onMouseOut={() => setVisible(false)}>
       <Icon className='tooltip-icon'></Icon>
-      <span>{props.message}</span>
+      <span className={`message ${visibleClass}`}>{props.message}</span>
     </div>
   )
 }
